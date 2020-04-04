22bfcad4329006850f33a637bb590fb92c7d0af6
root <root@localhost.localdomain>
2020-04-04 15:49:59 +0800

    update config

--- create.sh
+++ create.sh
@@ -1,8 +1,10 @@
 docker run --name web-proxy \
     --hostname webproxy \
     -d \
-    --mount type=bind,source="$(pwd)/conf.d",target=/etc/nginx/conf.d/ \
-    --mount type=bind,source="$(pwd)/wwwroot",target=/wwwroot \
     -p 80:80 \
     -p 443:443 \
+    --mount type=bind,source="$(pwd)/conf.d",target=/etc/nginx/conf.d/ \
+    --mount type=bind,source="$(pwd)/wwwroot",target=/wwwroot \
+    -p 10003:10003 \
+    -v /root/software:/app/software \
     nginx
