7e1cf5434f9b02988ff962f4ba2795a2c96cfc58
alan <alan.wei@live.com>
2020-04-04 14:49:20 +0800

    update

--- create.sh
+++ create.sh
@@ -1,1 +1,8 @@
-docker run --name web-proxy --hostname webproxy -d --mount type=bind,source="$(pwd)/conf.d",target=/etc/nginx/conf.d/ --mount type=bind,source="$(pwd)/wwwroot",target=/wwwroot -p 80:80 -p 443:443 nginx
+docker run --name web-proxy \
+    --hostname webproxy \
+    -d \
+    --mount type=bind,source="$(pwd)/conf.d",target=/etc/nginx/conf.d/ \
+    --mount type=bind,source="$(pwd)/wwwroot",target=/wwwroot \
+    -p 80:80 \
+    -p 443:443 \
+    nginx
